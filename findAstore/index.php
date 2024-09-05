ca7a55426daf20e731fcab94cf6b507e9fb89d8a
Steve119-64bits <stevechan0991@gmail.com>
2024-09-05 21:35:31 +0800

    update findAStore php

--- findAstore/index.php
+++ findAstore/index.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
 <html>
-    <head><title>Search a store</title>
-    <link rel="stylesheet" href="../WebStyle/mystyle.css">
+
+<head>
+    <title>Search a store</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../WebStyle/mystyle.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
-<section id="findbranch">
-<div class="branch">
-<h1>Find the nearest "JavaRoma"! Sleep</h1>
-<img src="../images/branch.png">
-</div>
-<?php include('../includes/navigationList.php');?>
-<?php include ('header.php');?>
-<div class="search">
-<form action="search.php" method="POST">
-    <input type="text" name="search" placeholder="Find a store">
-    <button type="submit" name="submit-search">Search</button>
-</form>
-</div>
-</section>
-<?php include('../includes/footerPolicy.php'); ?>
+    <section id="findbranch">
+        <div class="branch">
+            <h1>Find the nearest "JavaRoma"!</h1>
+            <img src="../images/branch.png">
+        </div>
+        <?php include('../includes/navigationList.php'); ?>
+        <?php include('header.php'); ?>
+        <div class="search">
+            <form action="search.php" method="POST">
+                <input type="text" name="search" placeholder="Find a store">
+                <button type="submit" name="submit-search">Search</button>
+            </form>
+        </div>
+    </section>
+    <?php include('../includes/footerPolicy.php'); ?>
 </body>
+
 </html>
