582858704651dbf8157f593f27db862f385035a2
Adrian Lim <adrianlim0120@gmail.com>
2024-09-06 02:31:25 +0800

    Revert "Merge branch 'main' of https://github.com/AlwinChong02/Javaroma"
    This reverts commit 08e0bed42a1c9ccf53d8af9b00efcea8e81caef4, reversing
    changes made to 964aac1a5779d04cfb58760f14e200848e77aef1.

--- login/register.php
+++ login/register.php
@@ -7,34 +7,35 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class="left">
-            <div class="illustration">
-                <img src="illustration.png" alt="Illustration">
-            </div>
+    <h1> Register Now to get amazing benefits!</h1>
+    <div class="left">
+        <div class="illustration">
+            <img src="illustration.png" alt="Illustration">
         </div>
-        <div class="right">
-            <div class="login-box">
-                <h1>Register</h1>
-                <p>Have existing account? <a href="login.php">Login now!</a></p>
+    </div>
+    <form id="register-form" action="register.php" method="post" onsubmit="validateForm()">
+        <div class="input-box">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="input-box">
+            <label for="email">Email:</label>
+            <input type="text" name="email" id="email">
+        </div>
+        <div class="input-box">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <button type="submit" onsubmit="validateForm()" class="login-btn">Register</button>
 
-                <form id="register-form" action="register.php" method="post" onsubmit="validateForm()">
-                    <div class="input-box">
-                        <label for="name">Name:</label>
-                        <input type="text" name="name" id="name">
-                    </div>
-                    <div class="input-box">
-                        <label for="email">Email:</label>
-                        <input type="text" name="email" id="email">
-                    </div>
-                    <div class="input-box">
-                        <label for="password">Password:</label>
-                        <input type="password" name="password" id="password">
-                    </div>
-                    <button type="submit" onsubmit="validateForm()" class="login-btn">Register</button>
-                </form>
-                <script src='userValidation.js'></script>
-                <?php
+        <script src='userValidation.js'></script>
+    </form>
+    <div>
+        <p>Already have an account? <a href="login.php">Login here</a></p>
+    </div>
+
+    <script src='userValidation.js'></script>
+    <?php
     $servername = "localhost";
     $serverUsername = "root";
     $serverPassword = "";
@@ -49,39 +50,32 @@
     }
 
     //get data from form 
-    if($_SERVER["REQUEST_METHOD"] === "POST") {
-        $name = $_POST['name'] ?? '';
-        $email = $_POST['email'] ?? '';
-        $password = $_POST['password'] ?? '';
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
 
-        //hash password
-        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+    //hash password
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 
-        //prepare and bind 
-        $sql = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
-        $sql->bind_param("sss", $name, $email, $hashedPassword);
-        $sql->execute();
+    // prepare and bind 
+    $sql = $conn->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+    $sql->bind_param("sss", $name, $email, $hashedPassword);
+    $sql->execute();
 
-        if ($sql->affected_rows > 0) {
-            //display pop up dialog
-            echo "<script>alert('Registration successful')</script>";
+    if ($sql->affected_rows > 0) {
+        //display pop up dialog
+        echo "<script>alert('Registration successful')</script>";
 
-            //back to login page
-            echo "<a href='login.php'>Login</a>";
-        } else {
-            echo "Registration failed";
-        }
 
-        $conn->close();
+        //back to login page
+        echo "<a href='login.php'>Login</a>";
+    } else {
+        echo "Registration failed";
     }
 
+    $conn->close();
+
     ?>
-
-            </div>
-        </div>
-    </div>
-
-    <script src='userValidation.js'></script>
 </body>
 
 </html>
