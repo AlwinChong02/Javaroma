a404e7e69b5b8f5887eeea9e23e72cce8cd0e8ba
Adrian Lim <adrianlim0120@gmail.com>
2024-09-06 22:10:31 +0800

    update logout.php

--- logout/index.php
+++ logout/index.php
@@ -5,8 +5,12 @@ $_SESSION = array();
 
 session_destroy();
 
-setcookie("username", "", time() - 3600, "/");
 setcookie("PHPSESSID", "", time() - 3600, "/");
+setcookie("userID", "", time() - 3600, "/");
+setcookie("username", "", time() - 3600, "/");
+setcookie("email", "", time() - 3600, "/");
+setcookie("password", "", time() - 3600, "/");
+
 
 header("Location:index.php"); 
 exit;
