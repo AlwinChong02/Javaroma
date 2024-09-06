de10bcf34161eb1a1bb8df3cda95e1e9777a310d
Adrian Lim <adrianlim0120@gmail.com>
2024-09-06 19:04:10 +0800

    add login at navigation

--- includes/navigationList.php
+++ includes/navigationList.php
@@ -7,5 +7,6 @@
                 <a href="/Javaroma/contactPage">Contact Page</a>
                 <a href="/Javaroma/rewardPage" id="rewardBtn">Rewards</a>
                 <a href="/Javaroma/findAstore/index.php">Find a Store</a>
+                <a href="/Javaroma/login/login.php" class="logo"><img src='/Javaroma/images/profile_icon.jpg' style="max-width: 18px;"/></a>
             </nav>
         </header>
