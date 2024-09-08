0fa0b916f2c610d7e4dcdebbef628c73a8985690
Adrian Lim <adrianlim0120@gmail.com>
2024-09-08 17:14:01 +0800

    update profile so that couldnt see when no login

--- account/profile.php
+++ account/profile.php
@@ -1,3 +1,55 @@
+<?php
+session_start();
+$servername = "localhost";
+$serverUsername = "root";
+$serverPassword = "";
+$dbname = "javaroma_db";
+
+//check conncection
+$conn = new mysqli($servername, $serverUsername, $serverPassword, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_SESSION['userID'])) {
+    $userID = $_SESSION["userID"];
+} else {
+    die("Error: User not logged in. Please log in to view your profile.");
+}
+
+//get data from form
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $newPassword = $_POST['password'] ?? '';
+
+    $newPassword = md5($newPassword);
+
+
+    //get userID from cookie
+    $userID = $_COOKIE['userID'];
+
+    //update query
+    $sql = $conn->prepare("UPDATE users SET username=?, email=?, password=? WHERE userID=?");
+    $sql->bind_param("sssi", $name, $email, $newPassword, $userID);
+    $sql->execute();
+
+    if ($sql->affected_rows <= 0) {
+        echo "Failed to update user: " . $conn->error;
+    } else {
+        // show a dialog box to inform user that the data has been updated
+        //reset cookies
+        setcookie('username', $name, time() + (86400 * 30), "/");
+        setcookie('email', $email, time() + (86400 * 30), "/");
+        setcookie('password', $newPassword, time() + (86400 * 30), "/");
+
+        echo "<script>alert('Your user information has been updated');</script>";
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <>
@@ -7,76 +59,35 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' type='text/css' media='screen' href='profile.css'>
     <style>
-        h1{
+        h1 {
             margin-top: 120px;
         }
     </style>
-</head>
-<body>
-    <?php include('../includes/navigationList.php'); ?>
-    <h1>Update User Profile</h1>
-    <!-- mystlye.css with profile.css -->
-    <form id='form-profile' action='profile.php' method='post' onsubmit='return validateForm()'>
-        
-        <div>
-        <label for='name'>Name:</label>
-        <input type='text' name='name' id='name'>
-        </div>
-        <div>
-        <label for='email'>Email:</label>
-        <input type='text' name='email' id='email'>
-        </div>
-        <div>
-        <label for='password'>Password:</label>
-        <input type='password' name='password' id='password'>
-        </div>
-        <button type='submit' class='save-btn'>Save Changes</button>
-    </form>
-    <script src='userValidation.js'></script>
-        <?php
-            $servername = "localhost";
-            $serverUsername = "root";
-            $serverPassword = "";
-            $dbname = "javaroma_db";
+    </head>
 
-            //check conncection
-            $conn = new mysqli($servername, $serverUsername, $serverPassword, $dbname);
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            } 
+    <body>
+        <?php include('../includes/navigationList.php'); ?>
+        <h1>Update User Profile</h1>
+        <!-- mystlye.css with profile.css -->
+        <form id='form-profile' action='profile.php' method='post' onsubmit='return validateForm()'>
 
-            //get data from form
-            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            <div>
+                <label for='name'>Name:</label>
+                <input type='text' name='name' id='name'>
+            </div>
+            <div>
+                <label for='email'>Email:</label>
+                <input type='text' name='email' id='email'>
+            </div>
+            <div>
+                <label for='password'>Password:</label>
+                <input type='password' name='password' id='password'>
+            </div>
+            <button type='submit' class='save-btn'>Save Changes</button>
+        </form>
+        <script src='userValidation.js'></script>
 
-                $name = $_POST['name'] ?? '';
-                $email = $_POST['email'] ?? '';
-                $newPassword = $_POST['password'] ?? '';
+        <?php include('../includes/footerPolicy.php'); ?>
+    </body>
 
-                $newPassword = md5($newPassword);
-
-
-                //get userID from cookie
-                $userID = $_COOKIE['userID'];
-
-                //update query
-                $sql= $conn->prepare("UPDATE users SET username=?, email=?, password=? WHERE userID=?");
-                $sql->bind_param("sssi", $name, $email, $newPassword, $userID);
-                $sql->execute();
-
-                if ($sql->affected_rows <= 0) {
-                    echo "Failed to update user: " . $conn->error;
-                } else {
-                    // show a dialog box to inform user that the data has been updated
-                    //reset cookies
-                    setcookie('username', $name, time() + (86400 * 30), "/");
-                    setcookie('email', $email, time() + (86400 * 30), "/");
-                    setcookie('password', $newPassword, time() + (86400 * 30), "/");
-
-                    echo "<script>alert('Your user information has been updated');</script>";
-                }
-
-            }
-            ?>
-    <?php include('../includes/footerPolicy.php'); ?>
-</body>
 </html>
